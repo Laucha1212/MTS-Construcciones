acda94dfeadeec1a0145c665d6f566020ad8fe19
NoeliaR <rodrigueznoeliasabrina@gmail.com>
2021-12-02 08:01:40 -0300

    Agrego imagenes para galeria

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ $(window).scroll(function() {
         $('.navbar-fixed-top').removeClass("navbar-fixed-top-sticky");
     }
 });
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+      return false;
+    });
+    
+    $(function() {
+        $( "#button" ).click(function() {
+          $( "#button" ).addClass( "onclic", 250, validate);
+        });
+      
+        function validate() {
+          setTimeout(function() {
+            $( "#button" ).removeClass( "onclic" );
+            $( "#button" ).addClass( "validate", 450, callback );
+          }, 2250 );
+        }
+          function callback() {
+            setTimeout(function() {
+              $( "#button" ).removeClass( "validate" );
+            }, 1250 );
+          }
+        });
+
+    
+  });
+
+
+  
