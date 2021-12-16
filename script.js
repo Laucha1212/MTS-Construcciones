e7535ad7f3491ca05a143489d1a3ee6d5ac385a6
Laucha1212 <67768232+Laucha1212@users.noreply.github.com>
2021-12-15 22:25:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-$(window).scroll(function() {
-    if( $(this).scrollTop() > 50 ) {
-        $('.navbar-fixed-top').addClass("navbar-fixed-top-sticky");
-    } else {
-        $('.navbar-fixed-top').removeClass("navbar-fixed-top-sticky");
+
+
+window.onscroll = () => {
+    let currentscrollpos = window.pageYOffset;
+    //MOSTRAR Y OCULTAR MENU
+
+    if (prevScrollpos > currentscrollpos) {
+        navbar.style.top = "0";
+        navbar.style.transition = "0.5s";
+    } else{
+        navbar.style.top = "-60px";
+        navbar.style.transition = "0.5s";
     }
-});
 
-$(document).ready(function() {
-    $('a[href^="#"]').click(function() {
-      var destino = $(this.hash);
-      if (destino.length == 0) {
-        destino = $('a[name="' + this.hash.substr(1) + '"]');
-      }
-      if (destino.length == 0) {
-        destino = $('html');
-      }
-      $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
-      return false;
-    });
+    prevScrollpos = currentscrollpos;
+
+    //mostrar y ocultar scroll estilos
+
+    let arriba = window.pageYOffset;
+
+    if(arriba <= 80) {
+        //containerMenu.style.borderBottom = "none";
     
-    $(function() {
-        $( "#button" ).click(function() {
-          $( "#button" ).addClass( "onclic", 250, validate);
-        });
-      
-        function validate() {
-          setTimeout(function() {
-            $( "#button" ).removeClass( "onclic" );
-            $( "#button" ).addClass( "validate", 450, callback );
-          }, 2250 );
-        }
-          function callback() {
-            setTimeout(function() {
-              $( "#button" ).removeClass( "validate" );
-            }, 1250 );
-          }
-        });
+        goTop.style.right = "-100%";
+    } else{
+        //containerMenu.style.borderBottom = "3px solid #ff2e63"
 
-    
-  });
-
-
-  
+        goTop.style.right = "0";
+        goTop.style.transition = "0.5s";
+    }
+}
